98dfeb8658673dcb1f17d08871085faae078ead2
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2025-04-21 02:57:21 -0500

    build con dominio

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ee1ae2032d2b88fa96e785e6b380975a
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "c1e9bb48bf3f5ae411f0203a30359001",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "5ffebdd02512f90342b3cd66dbf666d8",
+"flutter_bootstrap.js": "29f8c1a8ecf8334888bef02b79b65f5e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
